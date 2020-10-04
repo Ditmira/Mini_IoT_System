ee031520c6895717f65dfc0222fd068b00165e77
Ditmira <ditmiragjini@gmail.com>
2020-10-04 11:26:03 +0200

    Add MongoDB annotation

--- src/main/java/com/iotsystem/iotproject/Entity/Device.java
+++ src/main/java/com/iotsystem/iotproject/Entity/Device.java
@@ -2,6 +2,7 @@ package com.iotsystem.iotproject.Entity;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -17,6 +18,6 @@ public class Device {
     private int id;
     private String deviceType;
     private String deviceState;
-    
+    @DBRef
     private List<Attribute> attributes;
 }
