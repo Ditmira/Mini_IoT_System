62bc6c281c83cc7864236e4cd9889a2bdb8aca50
Ditmira <ditmiragjini@gmail.com>
2020-10-15 07:21:31 +0200

    Added dependecies for web socket, created new model, and controller

--- src/main/java/com/iotsystem/iotproject/Controller/WebController.java
+++ src/main/java/com/iotsystem/iotproject/Controller/WebController.java
@@ -1,10 +1,14 @@
 package com.iotsystem.iotproject.Controller;
 
 import com.iotsystem.iotproject.Entity.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebController {
+    @MessageMapping("/chat")
+    @SendTo("topic/messages")
     public Message send(Message message) {
         return new Message(message.getAttributeName(), message.getMaxValue(), message.getActualValue());
     }
